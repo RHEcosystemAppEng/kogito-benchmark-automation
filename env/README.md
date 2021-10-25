22eb555136cf10e9774a880972d7dd20d418d74a
ute-egozi <uegozi@redhat.com>
2021-10-25 20:05:45 +0300

    extracted roles for containers with podman or docker

--- env/README.md
+++
@@ -1,7 +0,0 @@
-###Inventories for different environment configurations
-- inventory-local: environment for the tester client and the application on the same machine the playbooks are executed from
-- inventory-VM1-tester: remote environment VM1 of the tester client
-- inventory-VM2-app: remote environment VM2 of the application
-- inventory-VM2-tester: remote environment VM2 of tester in case tester and app run on same remote machine 
-
-To setup which inventory is chosen, edit file `/testRunner/test-resources/batch.json`
--- testRunner/runTests.sh
+++ testRunner/runTests.sh
@@ -2,11 +2,9 @@
 
 BATCH_FILE=test-resources/batch.json
 
-INFRA=$(jq '.Infra' $BATCH_FILE)
 TESTER=$(jq '.Tester' $BATCH_FILE)
 APP=$(jq '.App' $BATCH_FILE)
 
-echo "defined INFRA: "$INFRA
 echo "TESTER env: "$TESTER
 echo "APP env: "$APP
 
@@ -18,20 +16,20 @@ CMD_LINE_PARAMS_APP=" -v "
 if [[ $TESTER = '"LOCAL"' ]] && [[ $APP = '"LOCAL"' ]]
 then
   echo "running locally"
-  INVENTORY_TESTER="../env/inventory-local"
+  INVENTORY_TESTER="env/inventory-local"
   CMD_LINE_PARAMS_TESTER=" -v "
-  INVENTORY_APP="../env/inventory-local"
+  INVENTORY_APP="env/inventory-local"
   CMD_LINE_PARAMS_APP=" -v -K "
-elif [[ $TESTER = '"VM1"' ]] && [ $APP = '"VM2"' ]]
+elif [[ $TESTER = '"VM1"' ]] && [[ $APP = '"VM2"' ]]
 then
   echo "running client on VM1 and app on VM2"
-  INVENTORY_TESTER="../env/inventory-VM1-tester"
-  INVENTORY_APP="../env/inventory-VM2-app"
+  INVENTORY_TESTER="env/inventory-VM1-tester"
+  INVENTORY_APP="env/inventory-VM2-app"
 elif [[ $TESTER = '"VM2"' ]] && [[ $APP = '"VM2"' ]]
 then
   echo "running client and app on VM2"
-  INVENTORY_TESTER="../env/inventory-VM2-tester"
-  INVENTORY_APP="../env/inventory-VM2-app"
+  INVENTORY_TESTER="env/inventory-VM2-tester"
+  INVENTORY_APP="env/inventory-VM2-app"
 else
    echo "not a valid environment: $TESTER $APP"
    exit
@@ -42,37 +40,25 @@ echo "tester command line parameters: "$CMD_LINE_PARAMS_TESTER
 echo "app inventory: "$INVENTORY_APP
 echo "app command line parameters: "$CMD_LINE_PARAMS_APP
 
-#pre processing
-#setup remote JMeter for new test run - remove any previously created test-run folder
-ansible -i "$INVENTORY_TESTER" tester -m ansible.builtin.file -a 'path="{{install_dir}}/kogito-benchmark/test-clients/jmeter-client/test-run" state=absent'
-#copy all data connected to test (except the data specific per each test run) to JMeter of tester host
-ansible -i "$INVENTORY_TESTER" tester -m ansible.builtin.copy -a 'src='$BATCH_FILE' dest="{{install_dir}}/kogito-benchmark/test-clients/jmeter-client/test-run/"'
+# pre processing
+# setup remote benchmark host for test run - remove any previously created test-run folder
+ansible -i "$INVENTORY_TESTER" tester -m ansible.builtin.file -a 'path="{{install_dir}}/kogito-benchmark/test/test-run" state=absent'
+# copy new test data to benchmark host
+ansible -i "$INVENTORY_TESTER" tester -m ansible.builtin.copy -a 'src='$BATCH_FILE' dest="{{install_dir}}/kogito-benchmark/test/test-run/"'
 
 TEST_COUNTER=0
 NO_OF_TESTS=$(jq '.Tests | length' $BATCH_FILE)
-WITH_WARMUP=$(jq -r '.Warmup .enabled' $BATCH_FILE)
-#run tests - checks the env and kicks off the test
 while [ $TEST_COUNTER -lt $NO_OF_TESTS ]
 do
-  #install/deploy/run/validate env
-  #ansible-playbook prepare-local-or-VM.yml -i $INVENTORY_APP $CMD_LINE_PARAMS_APP -e "$INFRA"
-  #run warmup
-  if [ $WITH_WARMUP = "yes" ]
-  then
-    ansible -i "$INVENTORY_TESTER" tester -m ansible.builtin.shell -a "./runTestWarmup.sh "$TEST_COUNTER"; chdir=\"{{install_dir}}/kogito-benchmark/test-clients/jmeter-client\""
-  fi
-  #kickoff metrics collection - call Lokeshs REST API here - send interval for polling metrics on application, env (Vm or OCP) to use
-  #TODO
-  #run test - could also be an ad hoc like the warmup call
-  ansible-playbook runtest-local-or-VM.yml -i $INVENTORY_TESTER $CMD_LINE_PARAMS_TESTER -e "testidx=$TEST_COUNTER"
-  #request accumulated metrics - call Lokeshs REST API here
-  #TODO
+  # install/deploy/run
+  ansible-playbook prepare-local-or-VM.yml -i $INVENTORY_APP $CMD_LINE_PARAMS_APP -e "testdefs=$BATCH_FILE"
+  # run test - could also be an ad hoc
+  ansible-playbook runtest-local-or-VM.yml -i $INVENTORY_TESTER $CMD_LINE_PARAMS_TESTER -e "testclient=JMETER testidx=$TEST_COUNTER"
   TEST_COUNTER=$((TEST_COUNTER+1))
 done
 
-#post processing
-#copy the remote test_run folder to the reportGenerator after removing any existing
+# post processing
 ansible -i "$INVENTORY_TESTER" tester -m ansible.builtin.file -a 'path="../reportGenerator/test-run" state=absent'
-ansible -i "$INVENTORY_TESTER" tester -m ansible.builtin.copy -a 'src="{{install_dir}}/kogito-benchmark/test-clients/jmeter-client/test-run" dest=../reportGenerator'
-#kickoff Danieles reporting here???
+ansible -i "$INVENTORY_TESTER" tester -m ansible.builtin.copy -a 'src="{{install_dir}}/kogito-benchmark/test/test-run" dest=../reportGenerator'
+# kickoff Danieles reporting here???
 #TODO
